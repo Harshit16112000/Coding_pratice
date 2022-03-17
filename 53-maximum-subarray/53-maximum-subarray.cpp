810bfa26ea73ef74ac36a8cd835188ac91dc5c5b
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2022-03-17 18:43:15 +0530

    Time: 96 ms (96.62%), Space: 67.8 MB (11.87%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+         int sum = 0;
+        int mx = INT_MIN;
+             for(auto it: nums)
+             {
+                 sum += it;
+                 mx = max(sum,mx);
+                 if(sum<0)
+                     sum =0;
+             }
+             return mx;
+    }
+};
