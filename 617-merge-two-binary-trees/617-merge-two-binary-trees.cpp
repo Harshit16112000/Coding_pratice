1e9a00a895e0536cc5c97308d57fb23f584a9290
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2022-03-17 18:25:29 +0530

    Time: 36 ms (89.39%), Space: 32.3 MB (63.23%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,33 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2)
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2)
     {
-       //  TreeNode* merged;
-//         if(root1 == NULL && root2 == NULL )
-//               return root1;
-//          else if(root1 == NULL)
-//               root1->val = root2->val;
-//         else if(root2 == NULL)
-//              root1->val = root1->val;
-//         else
-//         {
-//              root1->val += root2->val;
-           
-//         }
-//         for(int i=0;i<2;i++)
-//         {
-//             mergeTrees(root1->left, root2->left);
-//             mergeTrees(root1->right, root2->right);
-//         }
-//        // return merged;
-//        return nullptr;
+          if(root1 == NULL && root2 == NULL )
+              return root1;
+         else if(root1 == NULL)
+              return root2;
+        else if(root2 == NULL)
+             return root1;
+    
         
-         if (!t1 || !t2) return t1 ? t1 : t2;
+             root1->val += root2->val;
+           
+        
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        
+       // return merged;
+       return root1;
+        
+//          if (!t1 || !t2) return t1 ? t1 : t2;
 
-        TreeNode* node = new TreeNode(t1->val + t2->val);
-        node->left = mergeTrees(t1->left, t2->left);
-        node->right = mergeTrees(t1->right, t2->right);
-        return node;
+//         TreeNode* node = new TreeNode(t1->val + t2->val);
+//         node->left = mergeTrees(t1->left, t2->left);
+//         node->right = mergeTrees(t1->right, t2->right);
+//         return node;
     }
 };
