46f40238a6e3829af2a394243d5130af1e1e48af
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2022-04-03 18:38:32 +0530

    Time: 48 ms (22.68%), Space: 8.8 MB (35.52%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        vector<int> ans;
+        map<char,int> mp;
+        for(auto &x: p)
+            mp[x]++;
+        int count  = mp.size();
+        
+        int i=0,j=0;
+        int k = p.size();
+        while(j < s.size())
+        {
+            if(mp.find(s[j]) != mp.end())
+            {   
+                mp[s[j]]--;
+             if(mp[s[j]] == 0)
+                   count--;
+            }      
+             if(j-i + 1 < k)
+                   j++;
+          else if(j-i + 1 == k)
+            {
+                if(count == 0)
+                {    
+                    ans.push_back(i);
+                    cout << i;
+                }
+                if(mp.find(s[i]) != mp.end())
+                {
+                    mp[s[i]]++;
+                    if (mp[s[i]] == 1)
+                    {
+                    count++;
+                    }
+                
+                }
+                 i++;
+                 j++;
+                cout << count;       
+              }
+        }
+        return ans;
+    }
+};
