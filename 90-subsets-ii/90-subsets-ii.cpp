681f289844338155a1d02031d5e650019ba9adbf
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2022-04-11 22:45:13 +0530

    Time: 3 ms (80.13%), Space: 7.8 MB (57.28%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+     vector<vector<int>> result;
+    void answer(vector<int>& nums , int start , vector<int>& subset){
+        result.push_back(subset);
+        for(int i=start ;i<nums.size();i++){
+            if(i>start and nums[i-1] == nums[i])
+                continue;
+            subset.push_back(nums[i]);
+            answer(nums , i+1 , subset);
+            subset.pop_back();
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        vector<int> subset;
+        answer(nums, 0 , subset);
+        return result;
+    }
+};
