6f7e74cf7fa1d26cf35def19aa96eb4b2152c151
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2022-04-14 00:29:31 +0530

    Time: 13 ms (87.27%), Space: 10.4 MB (72.65%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,65 +1,21 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& nums, int target) {
-                sort(nums.begin(),nums.end());
-        
-        // two pointer approach 
-        long long mod=1e9+7;
-        int n=nums.size();
-        int ans=0;
-        long long sum;
-        for(int i=0;i<n-2;i++)
-        {
-            int l=i+1;
-            int r=n-1;
-            
-            while(l<r)
-            {
-                sum=nums[l]+nums[i]+nums[r];
-             
-                if(sum==target)
-                {
-                      int count1=1;
-                      int count2=1;
-                    while(l<r && nums[l]==nums[l+1])
-                    {
-                        l++;
-                        count1++;
-                    }
-                    
-                    while(l<r && nums[r]==nums[r-1])
-                    {
-                        r--;
-                        count2++;
-                    }
-                    
-                    if(l==r)
-                    {
-                        ans = (ans%mod + (count1%mod *(count1-1)%mod)/2)%mod;
-                    }
-                    
-                    if(l!=r)
-                    {
-                        ans = (ans%mod + (count1%mod* count2%mod));
-                    }
-                    
-                    l++;
-                    r--;
-                }
-                else
-                {
-                    if(sum>target)
-                    {
-                        r--;
-                    }
-                    else
-                    {
-                        l++;
-                    }
-                }
+    int threeSumMulti(vector<int>& A, int target) {
+           unordered_map<int, long> c;
+        int mod = 1e9 + 7;
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (auto it : c)
+            for (auto it2 : c) {
+                int i = it.first, j = it2.first, k = target - i - j;
+                if (!c.count(k)) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (i < j && j < k)
+                    res += c[i] * c[j] * c[k];
             }
-        }
-        
-        return ans;
+        return res%mod;   
     }
 };
